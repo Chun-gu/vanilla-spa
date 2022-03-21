af8f0549bebf6658604b47c2ed68b3857f6aa1f6
Chun-gu <chungu.yi92@gmail.com>
2022-03-21 23:54:58 +0900

    ✨ productListPage 컴포넌트에서 product 정보 fetch

--- src/page/ProductListPage.js
+++ src/page/ProductListPage.js
@@ -1,12 +1,30 @@
+import { getData } from "../API.js";
+
 export default function ProductListPage({ $target }) {
   console.log("ProductListPage.js");
   const $page = document.createElement("div");
   $page.className = "product-list-page";
-  $page.innerHTML = "<h1>상품 목록</h1>";
+  $page.innerHTML = "<h1>바닐라 SPA 쇼핑몰</h1>";
+
+  const $main = document.createElement('main');
+  
+
+  this.setState = function (nextState) {
+    this.state = nextState;
+  };
+
+  const fetchProducts = async () => {
+    const products = await getData("mall");
+    this.setState(products);
+    console.log(this.state)
+  };
+
+  fetchProducts();
 
   const $modal = document.createElement("div");
   $modal.className = "product-detail";
-  $modal.innerHTML = `<h2>상품 상세 정보</h2>`;
+  $modal.innerHTML = `<h2>상품 상세 정보 모달</h2>`;
+  $modal.style.display = "none";
 
   this.render = () => {
     $target.appendChild($page);
