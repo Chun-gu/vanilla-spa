7fe6ae6c17fbc62b6aac86cd8cdd76c0dbb075f9
Chun-gu <chungu.yi92@gmail.com>
2022-03-22 22:34:39 +0900

    ✨ ProductListPage에 ProductList 렌더

--- src/page/ProductListPage.js
+++ src/page/ProductListPage.js
@@ -1,33 +1,37 @@
 import { getData } from "../API.js";
+import ProductList from "../components/ProductList.js";
 
 export default function ProductListPage({ $target }) {
   console.log("ProductListPage.js");
+
   const $page = document.createElement("div");
   $page.className = "product-list-page";
-  $page.innerHTML = "<h1>바닐라 SPA 쇼핑몰</h1>";
+  $page.innerHTML = "<header><h1>Vanilla JS SPA Shopping Mall</h1></header>";
 
-  const $main = document.createElement('main');
-  
+  const $main = document.createElement("main");
+  $page.appendChild($main);
 
-  this.setState = function (nextState) {
-    this.state = nextState;
+  this.setProductsState = (nextProductsState) => {
+    this.productsState = nextProductsState;
+    this.render();
   };
 
   const fetchProducts = async () => {
     const products = await getData("mall");
-    this.setState(products);
-    console.log(this.state)
+    this.setProductsState(products);
+    console.log(this.productsState);
   };
 
   fetchProducts();
 
-  const $modal = document.createElement("div");
-  $modal.className = "product-detail";
-  $modal.innerHTML = `<h2>상품 상세 정보 모달</h2>`;
-  $modal.style.display = "none";
+  // const $modal = document.createElement("div");
+  // $modal.className = "product-detail";
+  // $modal.innerHTML = `<h2>상품 상세 정보 모달</h2>`;
+  // $modal.style.display = "none";
 
   this.render = () => {
     $target.appendChild($page);
-    $target.appendChild($modal);
+    new ProductList({ $target: $main, initialState: this.productsState });
+    // $target.appendChild($modal);
   };
 }
